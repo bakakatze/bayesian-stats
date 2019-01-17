0c44186a153cd5a8fb9575f26729edf8d43d8fa8
bakakatze <andrew.adiguna.halim@outlook.com>
2019-01-17 15:12:55 +1300

    Update bayesian.R
    add notes

--- bayesian.R
+++ bayesian.R
@@ -12,7 +12,4 @@ plot3d(f, xlim = c(-100,100), ylim = c(-100,100), alpha = 0.8,
        xlab = "X", ylab = "Y", zlab = "f(x,y)")
 
 
-# Learn about Jeffrey's Prior (uninformative prior) here:
-# https://www.youtube.com/watch?v=S42N_6pQ5TA
-
-# chapter 2
+# page 27 -- 4.2 Prediction in the Bus Problem
