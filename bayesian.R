6a3c80f4876f071bf6e39b00fa884e793dfa6e22
bakakatze <andrew.adiguna.halim@outlook.com>
2019-01-30 14:53:58 +1300

    Update bayesian.R
    add some stuff

--- bayesian.R
+++ bayesian.R
@@ -1,29 +1,6 @@
 
 require(rms)
-
-#### 1. Frank Harrell's Bayes Analysis in Sequential Testing ####
-
-# Objective:
-# Want to detect an efficacy of mu > 0 or futility at mu < 0.05 (assuming minimum clinically significant differencet at 0.05)
-
-#### 1.1 Specification of Prior ====
-# Let's decide that the prior is a mixture of two normal distribution with mean 0.
-# SD1 is chosen that P(mu > 1) = 0.1
-# SD2 is chosen that P(mu > 0.25) = 0.05
-sd1 = 1/qnorm(1 - 0.1)
-sd2 = 0.25/qnorm(1 - 0.05)
-wt = 0.5  # 1:1 mixture
-
-pdensity = function(x) wt * dnorm(x, 0, sd1) + (1-wt) * dnorm(x, 0, sd2)
-
-x = seq(-3, 3, length = 200)
-plot(x, pdensity(x), type = 'l', xlab = 'Efficacy', ylab = 'prior degree of belief')
-
-#
-#### 1.2 Sequential Testing Simulation ====
-
-
-
+require(Hmisc)
 
 #
 #### 4.2 Prediction in the Bus Problem [Bayes' Box] ####
@@ -49,14 +26,74 @@ prob_tomorrow = sum(theta*post) # 42.87%
 
 
 #
-#### 5 Parameter Estimation: Analytical Methods ####
+#### 5. Parameter Estimation: Analytical Methods ####
 
 # Analytical methods only work on some cases.
 # If analytical methods do not work, we will use MCMC and JAGS!
 
+### Choice of different priors other than Uniform prior
+
+## 2. Emphasising the extremes:
+# p(theta) ~ theta^-0.5 * (1-theta)^-0.5
+# or.. inverse binomial
 
 
+## 3. Well informed prior:
+# p(theta) ~ theta^100 * (1-theta)^100
+# just put more weight to the prior
 
+
+## General Form = Beta Distribution ##
+# p(x|a, b) ~ x^(a-1) * (1-x)^(b-1)
+
+# when data is very large, the choice of prior does not matter much
+# however when data is small, the choice of prior can means a lot <<< this means we need more data
+
+#### 6. Summarising the Posterior Distribution ####
+
+# Summarising posterior distribution depends on the shape
+
+# if it is normal, then just do mean +- SD
+
+# if it has multiple peaks, then display mean, median, mode
+
+## To estimate mean median mode, you use loss functions
+# quadratic loss function = mean
+# absolute loss function = median
+# all-or-nothing loss function = mode
+
+### Computing Point Estimates from a Bayes' Box
+
+# posterior mean is simple to get:
+post_mean = sum(theta*post)
+
+# posterior mode:
+highest_prob = max(post)
+post_mode = theta[post == highest_prob]
+
+# posterior median:
+csF = cumsum(post)
+dist = abs(csF - 0.5)
+post_median = theta[dist == min(dist)]
+
+# Credible Intervals:
+# similar to median, just use 0.025 and 0.975
+
+#### 7. Hypothesis Testing and Model Selection ####
+
+# Let's assume the mean Systolic Blood Pressure (SBP) in the population is 120mmHg with SD = 15 mmHg
+# We do a trial on 100 sample and get a mean of 115.9 mmHg
+# Test the hypothesis that the mean is below 120 mmHg
+
+#### 8. Markov Chain Monte Carlo ####
+
+plot(theta, post, xlab = "theta", ylab = "posterior probability")
+
+# markov chain monte carlo is an algorithm that generates random samples of parameter values
+# drawn from the posterior distribution.
+
+
+#### 9. Just Another Gibbs Sampler (JAGS) ####
 
 
 
